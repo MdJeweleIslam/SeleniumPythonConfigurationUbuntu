5af92e57111aa78ad1630e377e091e4e3798cb01
Jewele <mdjeweleislam@gmail.com>
2023-02-23 16:57:50 +0600

    fixing bugs

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-!sudo apt update
+sudo apt update
 echo "###################################"
 echo "Installing Chromium Driver"
 echo "###################################"
-!sudo apt install chromium-driver
+sudo apt install chromium-driver
 echo "###################################"
 echo "Installing essential library"
 echo "###################################"
-!apt install -y libxss1 libappindicator1 libindicator7 libu2f-udev
+apt install -y libxss1 libappindicator1 libindicator7 libu2f-udev
 echo "###################################"
 echo "Downloading Google Chrome Current Stable Version"
 echo "###################################"
-!wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo "###################################"
 echo "Installing The Google Chrome Current Stable Version using DPKG"
 echo "###################################"
-!dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
 echo "###################################"
 echo "Downloading Chrome Driver according to the Stable version number"
 echo "###################################"
-!wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip
 echo "###################################"
 echo "Unzip the Chrome Driver in the working directory."
 echo "###################################"
-!unzip chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
 echo "###################################"
 echo "Installing the selenium using PIP"
 echo "###################################"
-!pip install selenium
+pip install selenium
